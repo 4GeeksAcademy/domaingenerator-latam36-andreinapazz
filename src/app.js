e57c11d9a14910f002737f5e66a865623039f14b
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2024-11-08 22:59:24 +0000

    second-commit

--- src/app.js
+++ src/app.js
@@ -5,20 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#domains").innerHTML = generateDomain();
+};
+
+let generateDomain = () => {
   let pronoun = ["our", "her", "the", "them"];
   let adj = ["amazing", "disgusting", "incredible", "unbelievable"];
   let place = ["lab", "kitchen", "mall", "island"];
   let extension = [".com", ".net", ".ve", ".gob"];
 
+  let domsHTML = "<ul>";
+
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < place.length; k++) {
         for (let l = 0; l < extension.length; l++) {
-          console.log(pronoun[i] + adj[j] + place[k] + extension[l]);
+          let a = pronoun[i];
+          let b = adj[j];
+          let c = place[k];
+          let d = extension[l];
+
+          let completeDomain = a + b + c + d;
+
+          domsHTML += "<li>" + completeDomain + "</li>";
         }
       }
     }
   }
+  return domsHTML;
 };
